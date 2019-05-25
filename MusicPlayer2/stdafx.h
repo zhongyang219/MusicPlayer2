7f9367d8ea098b125002420d9d3b10162127d999
Zhong Yang <zhongyang219@hotmail.com>
2019-05-25 17:28:09 +0800

    日期、版本号更新

--- MusicPlayer2/stdafx.h
+++ MusicPlayer2/stdafx.h
@@ -106,7 +106,7 @@ using std::map;
 #define MAX_SONG_NUM 9999		//播放列表中文件数量的最大值
 
 #define VERSION L"2.65"	//程序版本
-#define COMPILE_DATE L"2019/04/05"
+#define COMPILE_DATE L"2019/05/25"
 
 //任务栏图按钮
 #define IDT_PLAY_PAUSE 1321		//任务栏缩略图“播放/暂停”按钮的ID
