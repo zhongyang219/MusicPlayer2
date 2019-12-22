32a8684225d8f017eb511ca12f61904d05f214cd
Zhong Yang <zhongyang219@hotmail.com>
2019-12-22 14:32:10 +0800

    歌词批量下载时保存匹配的歌曲id

--- MusicPlayer2/LyricBatchDownloadDlg.cpp
+++ MusicPlayer2/LyricBatchDownloadDlg.cpp
@@ -341,6 +341,8 @@ UINT CLyricBatchDownloadDlg::ThreadFunc(LPVOID lpParam)
 			continue;
 		}
 
+        song_info_ori.song_id = down_list[best_matched].id;
+
 		//在歌词前面添加标签
 		CLyricDownloadCommon::AddLyricTag(lyric_str, down_list[best_matched].id, down_list[best_matched].title, down_list[best_matched].artist, down_list[best_matched].album);
 
