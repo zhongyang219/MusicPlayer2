d6afd2a61ca69e699e485b266ceca71fe4ca4d69
Zhong Yang <zhongyang219@hotmail.com>
2020-08-10 11:41:57 +0800

    更新

--- MusicPlayer2/AboutDlg.cpp
+++ MusicPlayer2/AboutDlg.cpp
@@ -145,14 +145,23 @@ void CAboutDlg::OnPaint()
     draw.GetDC()->FillSolidRect(m_rc_pic, RGB(212, 230, 255));
 
     //画背景图
-    CPoint start_point{ m_rc_pic.TopLeft() };
-    CSize size{ m_rc_pic.Size() };
-    draw.ImageDrawAreaConvert(CSize(m_about_pic.GetWidth(), m_about_pic.GetHeight()), start_point, size, CDrawCommon::StretchMode::FIT, true);
-    CRect rc_clip{ start_point, size };
-    rc_clip.DeflateRect(theApp.DPI(2), theApp.DPI(2));
-    draw.GetGraphics()->SetClip(CGdiPlusTool::CRectToGdiplusRect(rc_clip));     //通过设置剪辑区域裁剪掉边缘的几个像素，否则使用GDI+绘图将图像拉伸时边缘会有白边
-    draw.DrawImage(m_about_pic, m_rc_pic.TopLeft(), m_rc_pic.Size(), CDrawCommon::StretchMode::FIT, true);
-    draw.GetGraphics()->ResetClip();
+    CSize img_size{ m_about_pic.GetWidth(), m_about_pic.GetHeight() };
+    if (min(img_size.cx, img_size.cy) < min(m_rc_pic.Width(), m_rc_pic.Height()))       //如果图片要被放大，则使用GDI+绘图
+    {
+        CPoint start_point{ m_rc_pic.TopLeft() };
+        CSize size{ m_rc_pic.Size() };
+        draw.ImageDrawAreaConvert(CSize(m_about_pic.GetWidth(), m_about_pic.GetHeight()), start_point, size, CDrawCommon::StretchMode::FIT, true);
+        CRect rc_clip{ start_point, size };
+        rc_clip.DeflateRect(theApp.DPI(2), theApp.DPI(2));
+        draw.GetGraphics()->SetClip(CGdiPlusTool::CRectToGdiplusRect(rc_clip));     //通过设置剪辑区域裁剪掉边缘的几个像素，否则使用GDI+绘图将图像拉伸时边缘会有白边
+        draw.DrawImage(m_about_pic, m_rc_pic.TopLeft(), m_rc_pic.Size(), CDrawCommon::StretchMode::FIT, true);
+        draw.GetGraphics()->ResetClip();
+    }
+    else        //否则使用GDI绘图
+    {
+        draw.DrawBitmap(m_about_pic, m_rc_pic.TopLeft(), m_rc_pic.Size(), CDrawCommon::StretchMode::FIT);
+    }
+
 }
 
 
