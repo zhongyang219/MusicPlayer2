65b6778d14d112a18c2b59874aef6e510cbd3657
Zhong Yang <zhongyang219@hotmail.com>
2020-08-26 16:03:59 +0800

    版本号更新

--- MusicPlayer2/Define.h
+++ MusicPlayer2/Define.h
@@ -56,8 +56,8 @@ using _tstring = std::string;
 #define MAX_NUM_LENGTH 5000		//定义获取音频文件信息数量的最大值
 #define MAX_SONG_NUM 9999		//播放列表中文件数量的最大值
 
-#define VERSION L"2.69"	//程序版本
-#define COMPILE_DATE L"2020/04/15"
+#define VERSION L"2.70"	//程序版本
+#define COMPILE_DATE L"2020/08/26"  //最后编译日期
 
 //任务栏图按钮
 #define IDT_PLAY_PAUSE 1321		//任务栏缩略图“播放/暂停”按钮的ID
