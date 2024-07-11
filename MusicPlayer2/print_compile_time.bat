e74dc5a0e848982eeaf2fd3cb8def2d7b93d7900
lrisora <43085437+lrisora@users.noreply.github.com>
2024-07-11 12:21:15 +0800

    注释改为英文，维持ASCII编码

--- MusicPlayer2/print_compile_time.bat
+++ MusicPlayer2/print_compile_time.bat
@@ -1,23 +1,23 @@
 @echo off
 setlocal
 
-:: 获取本地日期时间
+:: Get the local date and time
 for /f "tokens=2 delims==." %%i in ('wmic os get localdatetime /value') do set datetime=%%i
-:: 提取各部分（cmd不区分大小写）
+:: Extract the parts (cmd is not case sensitive)
 set year=%datetime:~0,4%
 set month=%datetime:~4,2%
 set day=%datetime:~6,2%
 set hh=%datetime:~8,2%
 set mm=%datetime:~10,2%
 set ss=%datetime:~12,2%
-:: 格式化为 yyyy-MM-dd HH:mm:ss
+:: Formatted as yyyy-MM-dd HH:mm:ss
 set formatted_datetime=%year%-%month%-%day% %hh%:%mm%:%ss%
-:: 输出到 compile_time.txt 文件
+:: Output to compile_time.txt file
 echo %formatted_datetime% > compile_time.txt
 
-:: 获取当前的 Git 提交哈希 (不显示错误信息)
+:: Get the current Git commit hash (without showing error messages)
 for /f %%i in ('git rev-parse HEAD 2^>nul') do set commit_hash=%%i
-:: 如果git命令执行成功那么将提交哈希追加到 compile_time.txt 文件中
+:: If the git command is executed successfully, the commit hash is appended to the compile_time.txt file.
 if not "%commit_hash%"=="" (echo %commit_hash% >> compile_time.txt)
 
 endlocal
