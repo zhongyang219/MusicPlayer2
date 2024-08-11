4fe1e5f23b1e536289b3d41ac8b0fff01d21ceeb
Zhong Yang <zhongyang219@hotmail.com>
2024-08-11 14:50:53 +0800

    调整各个界面的布局以确保在使用系统标准标题栏时右上角的图标不会与其他界面元素重叠

--- MusicPlayer2/UIElement.cpp
+++ MusicPlayer2/UIElement.cpp
@@ -2733,8 +2733,17 @@ int UiElement::PlaceHolder::GetHeight(CRect parent_rect) const
 
 bool UiElement::PlaceHolder::IsHide() const
 {
+    //标题栏中的图标除了最小化和最大化/还原外是否都隐藏
+    bool is_all_titlebar_icon_hide = !theApp.m_app_setting_data.show_minimode_btn_in_titlebar
+        && !theApp.m_app_setting_data.show_fullscreen_btn_in_titlebar
+        && !theApp.m_app_setting_data.show_skin_btn_in_titlebar
+        && !theApp.m_app_setting_data.show_settings_btn_in_titlebar
+        && !theApp.m_app_setting_data.show_dark_light_btn_in_titlebar
+        ;
     //如果设置了“仅当使用系统标准标题栏时才显示”，并且没有使用系统标准标题栏，则不显示
-    return (show_when_use_system_titlebar && !theApp.m_app_setting_data.show_window_frame && !theApp.m_ui_data.full_screen);
+    return (show_when_use_system_titlebar
+        && !theApp.m_app_setting_data.show_window_frame
+        && (!theApp.m_ui_data.full_screen) || (theApp.m_ui_data.show_menu_bar && is_all_titlebar_icon_hide));
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////
