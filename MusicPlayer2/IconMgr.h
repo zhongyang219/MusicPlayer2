b6e722e330e53ac296201165ee987d41e56fd133
zhongyang219 <zhongyang219@hotmail.com>
2024-08-16 11:58:38 +0800

    添加桌面歌词工具栏中“背景穿透”图标

--- MusicPlayer2/IconMgr.h
+++ MusicPlayer2/IconMgr.h
@@ -103,6 +103,7 @@ public:
         IT_Karaoke,                 // 歌词卡拉OK效果
         IT_Refresh,                 // 刷新
         IT_NewFolder,               // 添加新文件夹
+        IT_Background,              // 背景穿透
 
         IT_Le_Tag_Insert,           // Le 插入时间标签
         IT_Le_Tag_Replace,          // Le 替换时间标签
