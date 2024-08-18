68867151a865856c0d1c170d0e5a59abf52bbff8
zhongyang219 <zhongyang219@hotmail.com>
2024-08-18 16:05:04 +0800

    将FixWrongFilePath从SongDataManager中移出，修正“修复错误的文件路径”可能会匹配到不存在的文件的问题

--- MusicPlayer2/SongDataManager.cpp
+++ MusicPlayer2/SongDataManager.cpp
@@ -240,8 +240,6 @@ void CSongDataManager::LoadSongData(std::wstring path)
                 ar >> song_info.total_discs;
             }
             m_song_data[song_info] = song_info;     // 将读取到的一首歌曲信息添加到映射容器中
-            
-            UpdateFileNameMap(song_info);
         }
     }
     catch (CArchiveException* exception)
@@ -401,7 +399,6 @@ void CSongDataManager::AddItem(const SongInfo& song)
     ASSERT(!song.file_path.empty());
     m_song_data[song] = song;
     m_song_data_modified = true;
-    UpdateFileNameMap(song);
 }
 
 bool CSongDataManager::RemoveItem(const SongKey& key)
@@ -469,58 +466,4 @@ void CSongDataManager::ChangeFilePath(const wstring& file_path, const wstring& n
             song.file_path = new_path;
         m_song_data[new_path] = song;
     }
-}
-
-void CSongDataManager::UpdateFileNameMap(const SongInfo& song)
-{
-    std::wstring file_name{ song.GetFileName() };
-    m_song_file_name_map[file_name].insert(song.file_path);
-}
-
-//计算两个字符串右侧匹配的字符数量
-static int CalcualteStringRightMatchedCharNum(const std::wstring& str1, const std::wstring& str2)
-{
-    size_t index1{ str1.size() - 1 };
-    size_t index2{ str2.size() - 1 };
-    int char_matched{};
-    for (; index1 >= 0 && index2 >= 0; index1--, index2--)
-    {
-        if (str1[index1] == str2[index2])
-            char_matched++;
-        else
-            break;
-    }
-    return char_matched;
-}
-
-bool CSongDataManager::FixWrongFilePath(wstring& file_path) const
-{
-    std::wstring file_name{ CFilePathHelper(file_path).GetFileName() };
-    bool fixed{ false };
-    auto iter = m_song_file_name_map.find(file_name);
-    if (iter != m_song_file_name_map.end())
-    {
-        if (iter->second.size() == 1)      //媒体库中同名的文件只有一个时，直接修改为该文件的路径
-        {
-            file_path = *iter->second.begin();
-            fixed = true;
-        }
-        else if (iter->second.size() > 1)   //媒体库中同名的文件有多个时，查找两个路径末尾相同字符数量最多的那项
-        {
-            std::wstring best_match_path;
-            int max_matched_char_mun{};
-            for (const auto& path : iter->second)
-            {
-                int cur_matched_char_num = CalcualteStringRightMatchedCharNum(file_path, path);
-                if (cur_matched_char_num > max_matched_char_mun)
-                {
-                    max_matched_char_mun = cur_matched_char_num;
-                    best_match_path = path;
-                }
-            }
-            file_path = best_match_path;
-            fixed = true;
-        }
-    }
-    return CCommon::FileExist(file_path);
 }
