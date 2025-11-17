2d14c8e0c79674f17c56fe6f4faf9e60f455dc4b
zhongyang219 <zhongyang219@hotmail.com>
2025-11-17 22:02:09 +0800

    生成编译时间的脚本更新，获取日期时间的方式不再使用wmic命令，改成在bat中执行单行powershell命令，解决最新的win11中无法wmic命令的问题

--- MusicPlayer2/print_compile_time.bat
+++ MusicPlayer2/print_compile_time.bat
@@ -1,23 +1,18 @@
 @echo off
 setlocal
 
-:: Get the local date and time
-for /f "tokens=2 delims==." %%i in ('wmic os get localdatetime /value') do set datetime=%%i
-:: Extract the parts (cmd is not case sensitive)
-set year=%datetime:~0,4%
-set month=%datetime:~4,2%
-set day=%datetime:~6,2%
-set hh=%datetime:~8,2%
-set mm=%datetime:~10,2%
-set ss=%datetime:~12,2%
-:: Formatted as yyyy-MM-dd HH:mm:ss
-set formatted_datetime=%year%-%month%-%day% %hh%:%mm%:%ss%
-:: Output to compile_time.txt file
+:: Get formated date time by PowerShell.
+for /f "tokens=*" %%i in ('powershell -Command "Get-Date -Format 'yyyy-MM-dd HH:mm:ss'"') do set "formatted_datetime=%%i"
+
+:: output to ompile_time.txt file.
 echo %formatted_datetime% > compile_time.txt
 
 :: Get the current Git commit hash (without showing error messages)
 for /f %%i in ('git rev-parse HEAD 2^>nul') do set commit_hash=%%i
+
 :: If the git command is executed successfully, the commit hash is appended to the compile_time.txt file.
-if not "%commit_hash%"=="" (echo %commit_hash% >> compile_time.txt)
+if not "%commit_hash%"=="" (
+    echo %commit_hash% >> compile_time.txt
+)
 
 endlocal
